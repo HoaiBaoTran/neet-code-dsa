32a6da4e3cbd34ee04db27c2c40e83938d106c05
Trần Hoài Bảo <hoaibaotran1104@gmail.com>
2025-11-12 14:09:21 +0700

    1 pass

--- copy_linked_list_with_random_pointer.py
+++ copy_linked_list_with_random_pointer.py
@@ -1,3 +1,4 @@
+import collections
 from typing import Optional
 
 from node import Node
@@ -5,38 +6,34 @@ from node import Node
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        curr = head
-        map = {None: None}
+        # 2 pass
+        # curr = head
+        # map = {None: None}
         
-        while curr:
-            new_list_node = Node(curr.val)
-            map[curr] = new_list_node
-            curr = curr.next
+        # while curr:
+        #     new_list_node = Node(curr.val)
+        #     map[curr] = new_list_node
+        #     curr = curr.next
             
-        curr = head
-        while curr:
-            copy = map[curr]
-            copy.next = map[curr.next]
-            copy.random = map[curr.random]
-            curr = curr.next
-            
-        return map[head]
-            
-            
-            # new_list_node = Node(curr.val)
-            # map[curr] = new_list_node
-            
-            # if curr.next and curr.next not in map:
-            #     next_node = Node(curr.next.val)
-            #     map[curr.next] = next_node
-            # new_list_node.next = map[curr.next]
-            
-            # if curr.random and curr.random not in map:
-            #     random_node = Node(curr.random.val)
-            #     map[curr.random] = random_node
-            # new_list_node.random = map[curr.random]
-                
-            # curr = curr.next
+        # curr = head
+        # while curr:
+        #     copy = map[curr]
+        #     copy.next = map[curr.next]
+        #     copy.random = map[curr.random]
+        #     curr = curr.next
             
         # return map[head]
+            
+        # 1 pass
+        map = collections.defaultdict(lambda: Node(0))
+        map[None] = None
+        
+        cur = head
+        while cur:
+            map[cur].val = cur.val
+            map[cur].next = map[cur.next]
+            map[cur].random = map[cur.random]
+            cur = cur.next
+            
+        return map[head]
     
