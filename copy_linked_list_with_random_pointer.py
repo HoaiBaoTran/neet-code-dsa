72ffce18fadd5b6cbcd655639d16deb013de4a57
Trần Hoài Bảo <hoaibaotran1104@gmail.com>
2025-11-12 14:01:07 +0700

    2 pass random node

--- copy_linked_list_with_random_pointer.py
+++ copy_linked_list_with_random_pointer.py
@@ -6,18 +6,37 @@ from node import Node
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         curr = head
-        map = {}
+        map = {None: None}
+        
         while curr:
             new_list_node = Node(curr.val)
-            
-            if curr.next not in map:
-                next_node = Node(curr.next.val)
-                map[curr.next] = next_node
-                
-            if curr.next not in map:
-                random_node = Node(curr.random.val)
-                map[curr.random] = random_node  
-                
             map[curr] = new_list_node
             curr = curr.next
+            
+        curr = head
+        while curr:
+            copy = map[curr]
+            copy.next = map[curr.next]
+            copy.random = map[curr.random]
+            curr = curr.next
+            
         return map[head]
+            
+            
+            # new_list_node = Node(curr.val)
+            # map[curr] = new_list_node
+            
+            # if curr.next and curr.next not in map:
+            #     next_node = Node(curr.next.val)
+            #     map[curr.next] = next_node
+            # new_list_node.next = map[curr.next]
+            
+            # if curr.random and curr.random not in map:
+            #     random_node = Node(curr.random.val)
+            #     map[curr.random] = random_node
+            # new_list_node.random = map[curr.random]
+                
+            # curr = curr.next
+            
+        # return map[head]
+    
